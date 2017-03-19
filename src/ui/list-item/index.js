95075da5cf1747b3107a0ada5478a00988ecf995
Ilia Reingold <iliareingold@Ilias-MacBook-Pro.local>
2017-03-19 18:30:14 -0400

    styling the list

--- src/ui/list-item/index.js
+++ src/ui/list-item/index.js
@@ -1,6 +1,22 @@
 import React from 'react';
 
-const ListItem = () =>
-  <div>List Item</div>;
+import styles from './styles.scss';
+import ellp from './ellp.png';
+
+const ListItem = props =>
+  <div className={styles.item}>
+    <div className={styles.top}>
+      <div className={styles.lane}>
+        lane {props.lane}
+      </div>
+      <img className={styles.ellp} src={ellp} alt="" />
+      <div className={styles.place}>
+        place {props.place}
+      </div>
+    </div>
+    <div className={styles.middle}>
+      {props.middle}
+    </div>
+  </div>;
 
 export default ListItem;
--- src/ui/searchbar/list.js
+++ src/ui/searchbar/list.js
@@ -4,10 +4,26 @@ import ListItem from '../list-item';
 
 const List = () =>
   <div>
-    <ListItem />
-    <ListItem />
-    <ListItem />
-    <ListItem />
+    <ListItem
+      lane="A"
+      place="eBlock ON & QC"
+      middle="2008 Honda Civic is live"
+    />
+    <ListItem
+      lane="B"
+      place="eBlock ON & QC"
+      middle="Ford 2014 150 is live"
+    />
+    <ListItem
+      lane="C"
+      place="eBlock ON & QC"
+      middle="2008 Honda Civic is live"
+    />
+    <ListItem
+      lane="D"
+      place="eBlock ON & QC"
+      middle="2008 Honda Civic is live"
+    />
   </div>;
 
 export default List;
