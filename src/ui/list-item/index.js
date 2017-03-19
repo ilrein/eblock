07c577f0ae92b66cfa429932148ea3837d3d4fff
Ilia Reingold <iliareingold@Ilias-MacBook-Pro.local>
2017-03-19 18:44:12 -0400

    added stylizing

--- src/ui/list-item/index.js
+++ src/ui/list-item/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
+import cx from 'classnames';
 
+import fonts from '../../fonts/fonts.scss';
 import styles from './styles.scss';
 import ellp from './ellp.png';
 
@@ -11,11 +13,17 @@ const ListItem = props =>
       </div>
       <img className={styles.ellp} src={ellp} alt="" />
       <div className={styles.place}>
-        place {props.place}
+        {props.place}
       </div>
     </div>
-    <div className={styles.middle}>
+    <div className={cx(styles.middle, fonts['promixanova-semibold'])}>
       {props.middle}
+    </div>
+    <div className={cx(styles.sameline, fonts['promixanova'])}>
+      <div>{props.bottom} items remaining</div>
+      <div className={styles.watchlist}>
+        {props.watchList ? `, ${props.watchList} are on your watchlist` : null}
+      </div>
     </div>
   </div>;
 
