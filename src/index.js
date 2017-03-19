f2746e7c1a9ed80cd6a9232fefe77788a67313d4
Ilia Reingold <iliareingold@Ilias-MacBook-Pro.local>
2017-03-19 16:50:28 -0400

    added min-width check

--- src/index.js
+++ src/index.js
@@ -1,16 +1,35 @@
 import React from 'react';
 import { render } from 'react-dom';
+import { WindowResizeListener } from 'react-window-resize-listener';
 
 import Navbar from './ui/navbar';
 
 import './normalize.css';
 
 class App extends React.Component {
+  state = { minWidthReached: false }
+
+  handleResize = (windowSize) => {
+    console.log(windowSize);
+    if (windowSize.windowWidth < 1200) {
+      this.setState({ minWidthReached: false })
+    } else {
+      this.setState({ minWidthReached: true })
+    }
+  }
+
   render() {
     return (
       <div>
-        <Navbar />
-        App
+        <WindowResizeListener onResize={windowSize => this.handleResize(windowSize)} />
+        {this.state.minWidthReached ?
+          <div>
+            <Navbar />
+            App
+          </div>
+          :
+          <div>Min width not reached</div>
+        }
       </div>
     )
   }
--- src/ui/navbar/index.js
+++ src/ui/navbar/index.js
@@ -10,10 +10,29 @@ import bars from './bars.png';
 const Navbar = () =>
   <div className={cx(styles.header, styles.black)}>
     <img src={logo} alt="eBlock" className={styles.logo} />
-    <div className={cx(styles.grey, styles.body)}>
-      <img src={bars} alt="eBlock" />
-      <div className={fonts['promixanova-semibold']}>BUY</div>
+
+    <div className={cx(styles.buttons)}>
+      <div className={cx(styles.grey, styles.body)}>
+        <img className={styles.icon} src={bars} alt="eBlock" />
+        <div className={cx(fonts['promixanova-semibold'], styles.content)}>BUY</div>
+      </div>
+
+      <div className={cx(styles.grey, styles.body)}>
+        <img className={styles.icon} src={bars} alt="eBlock" />
+        <div className={cx(fonts['promixanova-semibold'], styles.content)}>SELL</div>
+      </div>
+
+      <div className={cx(styles.grey, styles.body)}>
+        <img className={styles.icon} src={bars} alt="eBlock" />
+        <div className={cx(fonts['promixanova-semibold'], styles.content)}>PENDING</div>
+      </div>
+
+      <div className={cx(styles.grey, styles.body)}>
+        <img className={styles.icon} src={bars} alt="eBlock" />
+        <div className={cx(fonts['promixanova-semibold'], styles.content)}>FINISHED</div>
+      </div>
     </div>
+
   </div>;
 
 export default Navbar;
