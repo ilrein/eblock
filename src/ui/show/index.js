389eeadf310fe666030ba245eec092adb0dc9a45
Ilia Reingold <iliareingold@Ilias-MacBook-Pro.local>
2017-03-19 20:06:58 -0400

    added captions for show

--- src/ui/show/index.js
+++ src/ui/show/index.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
+import cx from 'classnames';
+import { Button } from 'semantic-ui-react';
 
+import fonts from '../../fonts/fonts.scss';
 import styles from './styles.scss';
 import car from '../lanes/car.png';
 import verified from '../lanes/verified.png';
@@ -17,6 +20,19 @@ export default class Show extends Component {
           <img className={styles.numcopy} src={numcopy} alt="" />
           <img className={styles.iconscopy} src={iconscopy} alt="" />
           <img className={styles.miles} src={miles} alt="" />
+        </div>
+        <div className={styles.details}>
+          <div className={styles.left}>
+            <div className={cx(styles.top, fonts['promixanova-semibold'])}>
+              #291 - 2004 Ford F-150 Supercab 145" FX4 4WD
+            </div>
+            <div className={cx(styles.bottom, fonts['promixanova'])}>
+              Pars Auto, Ottawa, ON
+            </div>
+          </div>
+          <div className={styles.right}>
+            <Button inverted color="green">Bid $13,000</Button>
+          </div>
         </div>
       </div>
     );
