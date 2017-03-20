16ed12d000dd8278fbe521ad007dcf629f98fd58
Ilia Reingold <iliareingold@Ilias-MacBook-Pro.local>
2017-03-19 20:19:50 -0400

    added table for show

--- src/ui/show/index.js
+++ src/ui/show/index.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import cx from 'classnames';
-import { Button } from 'semantic-ui-react';
+import { Button, Message, Label, Header, Image, Table  } from 'semantic-ui-react';
 
 import fonts from '../../fonts/fonts.scss';
 import styles from './styles.scss';
@@ -33,6 +33,149 @@ export default class Show extends Component {
           <div className={styles.right}>
             <Button inverted color="green">Bid $13,000</Button>
           </div>
+        </div>
+        <div className={styles.data}>
+          <Message color="green">
+            <b className={cx(styles.black, fonts['promixanova-semibold'])}>73/100</b>
+            <span className={styles.grey}> VEHICLE SCORE</span>
+          </Message>
+          <Message color="red">
+            <b className={cx(styles.black, fonts['promixanova-semibold'])}>$3,000</b>
+            <span className={styles.grey}> IN DAMAGE</span>
+          </Message>
+
+          <div>
+            <div className={cx(styles.caption, fonts['promixanova-semibold'])}>vehicle details</div>
+            <div className={styles.labels}>
+              <Label color="green">AM/FM</Label>
+              <Label>CONSOLE</Label>
+              <Label>ALLOW WEELS</Label>
+              <Label color="green">CD PLAYER</Label>
+              <Label color="green">DUAL AIRBAG</Label>
+              <Label color="green">ABS</Label>
+              <Label>CRUISE CONTROL</Label>
+            </div>
+          </div>
+
+          <Table basic='very' collapsing style={{ width: '100%' }}>
+            <Table.Body>
+              <Table.Row>
+                <Table.Cell>
+                  <Header as='h4'>
+                    <Header.Content>
+                      Year
+                    </Header.Content>
+                  </Header>
+                </Table.Cell>
+                <Table.Cell>
+                  2015
+                </Table.Cell>
+              </Table.Row>
+              <Table.Row>
+                <Table.Cell>
+                  <Header as='h4'>
+                    <Header.Content>
+                      Make
+                    </Header.Content>
+                  </Header>
+                </Table.Cell>
+                <Table.Cell>
+                  BMW
+                </Table.Cell>
+              </Table.Row>
+              <Table.Row>
+                <Table.Cell>
+                  <Header as='h4'>
+                    <Header.Content>
+                      Model
+                    </Header.Content>
+                  </Header>
+                </Table.Cell>
+                <Table.Cell>
+                  X3
+                </Table.Cell>
+              </Table.Row>
+
+              <Table.Row>
+                <Table.Cell>
+                  <Header as='h4'>
+                    <Header.Content>
+                      Trim
+                    </Header.Content>
+                  </Header>
+                </Table.Cell>
+                <Table.Cell>
+                  AWD 4dr
+                </Table.Cell>
+              </Table.Row>
+
+              <Table.Row>
+                <Table.Cell>
+                  <Header as='h4'>
+                    <Header.Content>
+                      VIN
+                    </Header.Content>
+                  </Header>
+                </Table.Cell>
+                <Table.Cell>
+                  JKDMFKDFIJ1212
+                </Table.Cell>
+              </Table.Row>
+
+              <Table.Row>
+                <Table.Cell>
+                  <Header as='h4'>
+                    <Header.Content>
+                      Stock #
+                    </Header.Content>
+                  </Header>
+                </Table.Cell>
+                <Table.Cell>
+                  C88S9A
+                </Table.Cell>
+              </Table.Row>
+
+              <Table.Row>
+                <Table.Cell>
+                  <Header as='h4'>
+                    <Header.Content>
+                      Vehicle Mileage
+                    </Header.Content>
+                  </Header>
+                </Table.Cell>
+                <Table.Cell>
+                  164,838km
+                </Table.Cell>
+              </Table.Row>
+
+              <Table.Row>
+                <Table.Cell>
+                  <Header as='h4'>
+                    <Header.Content>
+                      Doors
+                    </Header.Content>
+                  </Header>
+                </Table.Cell>
+                <Table.Cell>
+                  4
+                </Table.Cell>
+              </Table.Row>
+
+              <Table.Row>
+                <Table.Cell>
+                  <Header as='h4'>
+                    <Header.Content>
+                      Passengers
+                    </Header.Content>
+                  </Header>
+                </Table.Cell>
+                <Table.Cell>
+                  5
+                </Table.Cell>
+              </Table.Row>
+
+            </Table.Body>
+          </Table>
         </div>
       </div>
     );
